f1da4ffb9a710dd6f4cb1bf7938d685e2888abb0
LahiruDulanjaya <laahirudulanjaya@gmail.com>
2019-02-22 02:57:58 +0530

    jwt

--- Project Evaluvator/front-project-evaluator/src/App.js
+++ Project Evaluvator/front-project-evaluator/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import {createStore,applyMiddleware} from 'redux'
 import './App.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
@@ -7,14 +7,15 @@ import 'mdbreact/dist/css/mdb.css';
 import  Register from './Components/Auth/Register/Register'
 import Login from './Components/Auth/Login/Login'
 import{BrowserRouter as Router , Route, Switch} from 'react-router-dom'
+import store from './store'
 
+import {Provider } from 'react-redux'
 import Navbar from './Components/Navbar/Navbar';
 import PageNotFound from './Components/Error/PageNotFound';
-
 class App extends Component {
   render() {
     return (
-
+<Provider store={store}>
       <Router>
       <div className="App">
         <Navbar />
@@ -25,7 +26,7 @@ class App extends Component {
         </Switch>
       </div>
       </Router>
-
+      </Provider>
   
     );
   }
