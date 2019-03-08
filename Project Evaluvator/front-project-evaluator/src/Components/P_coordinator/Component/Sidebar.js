8bf04bca0e249a84fa2ce3c65e89254643758988
heshan <heshan.sandaruwan55875@gmail.com>
2019-03-08 18:19:11 +0530

    Side bar change

--- Project Evaluvator/front-project-evaluator/src/Components/P_coordinator/Component/Sidebar.js
+++ Project Evaluvator/front-project-evaluator/src/Components/P_coordinator/Component/Sidebar.js
@@ -9,32 +9,40 @@ const SideNavigation = () => {
                 <img alt="MDB React Logo" className="img-fluid" src={logo}/>
             </a> */}
             <MDBListGroup className="list-group-flush">
-                <NavLink exact={true} to="/pg" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon icon="chart-pie" className="mr-3"/>
-                        Dashboard
-                    </MDBListGroupItem>
-                </NavLink>
-               
-                <NavLink to="/Users" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon icon="users" className="mr-3"/>
-                        Users
-                    </MDBListGroupItem>
-                </NavLink>
-                <NavLink to="/pg/project" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon icon="file" className="mr-3"/>
-                        Projects
-                    </MDBListGroupItem>
-                </NavLink>
-                
-                <NavLink to="/404" activeClassName="activeClass">
-                    <MDBListGroupItem>
-                        <MDBIcon icon="exclamation" className="mr-3"/>
-                        404
-                    </MDBListGroupItem>
-                </NavLink>
+                <div className="container">
+                    <div className="row border border-grey border-right-0 border-left-0 border-top-0">
+                        <NavLink exact={true} to="/pg" activeClassName="activeClass">
+                            <MDBListGroupItem>
+                                <MDBIcon icon="chart-pie" className="mr-3"/>
+                                Dashboard
+                            </MDBListGroupItem>
+                        </NavLink>
+                    </div>
+                    <div className="row border border-grey border-right-0 border-left-0 border-top-0">          
+                        <NavLink to="/Users" activeClassName="activeClass">
+                            <MDBListGroupItem>
+                                <MDBIcon icon="users" className="mr-3"/>
+                                Users
+                            </MDBListGroupItem>
+                        </NavLink>
+                    </div>
+                    <div className="row border border-grey border-right-0 border-left-0 border-top-0">
+                        <NavLink to="/pg/project" activeClassName="activeClass">
+                            <MDBListGroupItem>
+                                <MDBIcon icon="file" className="mr-3"/>
+                                Projects
+                            </MDBListGroupItem>
+                        </NavLink>
+                    </div>
+                    <div className="row border border-grey border-right-0 border-left-0 border-top-0">
+                    <NavLink to="/404" activeClassName="activeClass">
+                        <MDBListGroupItem>
+                            <MDBIcon icon="exclamation" className="mr-3"/>
+                            404
+                        </MDBListGroupItem>
+                    </NavLink>
+                    </div>
+                </div>
             </MDBListGroup>
         </div>
     );
