b740bacbad6f1525838bcfb6d7e11d0eb886a4f5
manjitha-teshara <teranteshara01@gmail.com>
2019-03-17 14:31:30 +0530

    fix initial errors session coordinater

---
+++ Project Evaluvator/front-project-evaluator/src/Components/S_coordinator/Pages/P_milstone_form.js
@@ -0,0 +1,23 @@
+
+import TextField from '@material-ui/core/TextField';
+
+const Milstoneform = () => {
+    
+    
+        return (
+        <div>
+          <form  noValidate autoComplete="off">
+            <TextField
+              id="standard-name"
+              label="Name of the Milstone"
+              margin="normal"
+            />
+            
+           
+            </form>
+            </div>
+        )
+
+}
+export default Milstoneform
+
--- Project Evaluvator/front-project-evaluator/src/Components/S_coordinator/Scoordinator.js
+++ Project Evaluvator/front-project-evaluator/src/Components/S_coordinator/Scoordinator.js
@@ -1,26 +1,21 @@
 import React ,{ Component }  from 'react';
 import SideNavigation from './Component/Sidebar2'
 import { Route } from 'react-router-dom';
-import Calender from './Component/Calender'
+import Project from './Pages/Project/Project';
+import Calender from './Component/Calender';
 
-// import Project from './Pages/Project/Project'
-
-class Scoordinator extends Component{
+class Scoodinater extends Component{
     render(){
         return(
 
             <div className="row">
                 <div className="col-md-3">
                     <SideNavigation></SideNavigation>
-                    
                 </div>
-                <div className="col-md-9">
-                    <Calender></Calender>
-                </div>
-                <div className="col-md-3"></div>
+                <div className="col-md-9"><Calender></Calender></div>
             </div>
         )
     }
 } 
 
-export default Scoordinator
+export default Scoodinater
