2aac232dccf8d20b43857eb5e6563d0baeca068e
heshan <heshan.sandaruwan55875@gmail.com>
2019-04-07 14:54:16 +0530

    Color change

--- Project Evaluvator/front-project-evaluator/src/Components/P_coordinator/Pcoordinator.js
+++ Project Evaluvator/front-project-evaluator/src/Components/P_coordinator/Pcoordinator.js
@@ -2,12 +2,18 @@ import React ,{ Component }  from 'react';
 import SideNavigation from './Component/Sidebar2'
 import { Route } from 'react-router-dom';
 import Project from './Pages/Project/Project'
+
+var divStyle={
+    background:"#6699FF",
+    height: "700px",
+};
+
 class Pcoodinater extends Component{
     render(){
         return(
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-3">
+                    <div className="col-md-3" style={divStyle}>
                         <SideNavigation></SideNavigation>
                     </div>
                 </div>
