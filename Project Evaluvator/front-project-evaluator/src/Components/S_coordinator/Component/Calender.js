019ecbcaa30b54b2f71066f4034048fdd3e2fa51
manjitha-teshara <teranteshara01@gmail.com>
2019-04-07 14:36:35 +0530

    add react-calender

--- Project Evaluvator/front-project-evaluator/src/Components/S_coordinator/Component/Calender.js
+++ Project Evaluvator/front-project-evaluator/src/Components/S_coordinator/Component/Calender.js
@@ -1,62 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
-import { NavLink } from 'react-router-dom';
-import BigCalendar from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
+import React from "react";
+import { MDBRow, MDBCol, MDBBtn } from "mdbreact";
+import { MDBContainer } from "mdbreact";
+import Calendar from "react-calendar";
 
-const myEventsList=["first event","second event","third event"];
+ 
+class Calender extends React.Component {
+    state = {
+    date: new Date(),
+    
+  }
+ 
+  onChange = date => this.setState({ date });
+  
+  callDay = (clikedDay) => { console.log(clikedDay)};//moment(day.dateString).format(_format)
 
-const MyCalendar = props => (
-    <div style={{height:500}}>
-      <BigCalendar
-        localizer={localizer}
-        events={myEventsList}
-        startAccessor="start"
-        endAccessor="end"
-      />
-    </div>
-  )
-
-class Calender extends React.Component{
-    render() {
-        const { classes } = this.props;
-
-        return (
-          <div className="container">
-          <div className="row">
-            <div className="col-md-8">
-              <div style={{height:500}}>
-              <BigCalendar
-                localizer={localizer}
-                events={myEventsList}
-                startAccessor="start"
-                endAccessor="end"
-                />
-              </div>
-            </div>
-            <div className="col-md-4"></div>
-          </div>
-          </div>
-        );
-    }
-
+  render() {
+    return (
+      <div>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+          onClickDay={this.callDay}
+        />
+      </div>
+    );
+  }
 }
 
 
 export default Calender;
-
