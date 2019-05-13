2c22a202b9662892294e05d03d2757b95c2f7c4d
LahiruDulanjaya <laahirudulanjaya@gmail.com>
2019-05-13 19:12:28 +0530

    new

--- Project Evaluvator/front-project-evaluator/src/Components/P_coordinator/Pages/ManageProject/updateGroups.js
+++ Project Evaluvator/front-project-evaluator/src/Components/P_coordinator/Pages/ManageProject/updateGroups.js
@@ -3,7 +3,7 @@ import {getmilestones,updatemilestone,delemilestone} from '../../../../actions/m
 import {connect} from 'react-redux'
 import {getprojectnames} from '../../../../actions/ProjectActions'
 
-import { MDBTable, MDBTableBody, MDBTableHead ,MDBBtn,MDBDataTable} from 'mdbreact';
+import { MDBTable, MDBTableBody, MDBTableHead ,MDBBtn,MDBDataTable, Input} from 'mdbreact';
 import axios from'axios'
 import swal from 'sweetalert'
 import Button from '@material-ui/core/Button';
@@ -16,22 +16,65 @@ import _ from 'lodash'
 
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCol, MDBRow, MDBContainer,MDBIcon} from 'mdbreact';
 import { Card} from 'semantic-ui-react'
+var stud
 
 var cardStyle={
     backgroundColor: "#DFDFDF",
     size: 'sm'
   }
-
+var groupno
 class UpdateGroups extends React.Component{
   constructor(props){
     super(props)
     this.state={
       Projectname:'',
-       groups:[]
+       groups:[],
+       updategroups:[
+         {
+        groupno:'',
+        students:[{
+          Registrationnumber:'',
+          Name:''
+        }],
+        Supervisor:'',
+        Mentor:'',
+        open:false,
+        Registrationnumber:'',
+        Name:''
+
+         }
+       ]
 
     }
     this.handleChange =this.handleChange.bind(this)
+    this.onchange=this.onchange.bind(this)
+
   }
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  handleClickOpen = (group) => {
+    this.setState({ open: true });
+    this.setState(prevState => ({
+      updategroups: {
+          ...prevState.updategroups,
+          groupno: group.groupno,
+          students:group.students,
+          Supervisor:group.Supervisor,
+          Mentor:group.Mentor
+      }
+  }))
+  groupno=group.groupno
+  stud=group.students.map(stu=>
+    <div class="two fields">
+<Input value={stu.Registrationnumber} onChange={(e) => {this.onchange1(e, group.groupno,stu.Name)}} name="Registrationnumber" ></Input>
+<Input value={stu.Name} onChange={(e) => {this.onchange1(e, group.groupno,stu.Registrationnumber)}} name="Name"></Input>
+</div>
+)
+  
+    
+  };
+ 
   handleChange(e,data){
     
     let value = data.value
@@ -51,7 +94,60 @@ class UpdateGroups extends React.Component{
   componentDidMount(){
     this.props.getprojectnames()
   }
+  onchange1=(e,groupno,reg)=>{
+    var namee =e.target.name
+    var value =e.target.value
+var newgrp=[]
+    this.state.groups[0].groups.map(grp=>{
+      if(grp.groupno==groupno){
+        var student=[]
+        grp.students.map(st=>{
+          if(st.Registrationnumber==reg){
+            const students={
+              Registrationnumber:reg,
+              Name:value,
+              Projectname:this.state.Projectname
+            }
+            student.push(students)
+          }
+          else{
+            student.push(st)
+          }
+        })
+        var grup={
+          students:student,
+          groupno:grp.groupno,
+
+        }
+        newgrp.push(grup)
+      }
+      else{
+        newgrp.push(grp)
+      }
+    })
+    this.setState({updategroups:newgrp})
+    var arr=[]
+    arr.push({groups:newgrp})
+    this.setState({groups:arr});
+console.log(this.state)
+
+  }
+  onchange(e){
+    var namee =e.target.name
+    var value =e.target.value
+
+
+    this.setState(prevState => ({
+        updategroups: {
+          ...prevState.updategroups,
+          [namee]:value
+      }
+  }))
+  }
     render(){
+      console.log(this.state)
+
+       
         var stateOptions3=[]
   this.props.project.project.map(project=>{
     
@@ -63,6 +159,7 @@ class UpdateGroups extends React.Component{
     stateOptions3.push(val)
   })
         return(
+
     <div className="container">
       <div className="row">
 
@@ -70,7 +167,47 @@ class UpdateGroups extends React.Component{
         <Card className="ml-5" fluid color='orange' header='select project' />
                     <Dropdown placeholder='Select Project to Update Milestone'  selection options={stateOptions3}  value={this.state.Projectname} onChange={this.handleChange}/>
         <div className="ml-5 pt-2">
-        
+         <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          fullWidth={true}
+          maxWidth="sm"
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        ><DialogTitle id="alert-dialog-title">{"Update Milestones"}</DialogTitle>
+        <DialogContent>
+         
+         <Form>
+    <Form.Field>
+      <label>group Number</label>
+      <input value={this.state.updategroups.groupno} name="groupno" onChange={this.onchange}/>
+    </Form.Field>
+   
+    <Form.Field>
+      <label>Students</label>
+  
+      {stud}
+            </Form.Field>
+    <Form.Field>
+      <label>Supervisor</label>
+      <input value={this.state.updategroups.Supervisor} name="Supervisor"onChange={this.onchange} />
+    </Form.Field>
+    <Form.Field>
+      <label>Mentor</label>
+      <input value={this.state.updategroups.Mentor} name="Mentor"onChange={this.onchange} />
+    </Form.Field>
+   
+  </Form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose} color="primary">
+            Cansel
+          </Button>
+          <Button onClick={this.updateProject} color="primary" autoFocus>
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
         <MDBCard>
           <MDBCardBody style={cardStyle}>
           <MDBTable responsive>
@@ -102,7 +239,7 @@ class UpdateGroups extends React.Component{
                 <td></td>
                 
                 
-        <td><MDBIcon far icon="edit" className="indigo-text pr-3" size="2x" onClick={()=>this.handleClickOpen()} /></td>
+        <td><MDBIcon far icon="edit" className="indigo-text pr-3" size="2x" onClick={()=>this.handleClickOpen(group)} /></td>
        <td><MDBIcon icon="trash" className="red-text pr-3" size="2x"onClick={()=>this.ondelete()}/> </td>     
 
 
